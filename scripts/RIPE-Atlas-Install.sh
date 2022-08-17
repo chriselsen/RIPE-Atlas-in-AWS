02cc57dc18b2845fd763ada1e1c0298f7b1f35ba
Christian Elsen <chriselsen@users.noreply.github.com>
2022-08-17 10:46:18 -0700

    Update RIPE-Atlas-Install.sh

--- scripts/RIPE-Atlas-Install.sh
+++ scripts/RIPE-Atlas-Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install git tar fakeroot libssl-dev libcap2-bin autoconf automake libtool build-essential -y
+sudo apt update && sudo apt-get upgrade -y && sudo apt install git tar fakeroot libssl-dev libcap2-bin autoconf automake libtool build-essential -y
 cd /tmp
 git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git
 ./ripe-atlas-software-probe/build-config/debian/bin/make-deb
