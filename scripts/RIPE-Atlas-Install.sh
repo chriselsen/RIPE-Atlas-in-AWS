5f3e39830cfad28efeddeb315067733f03cf4b5e
Christian Elsen <chriselsen@users.noreply.github.com>
2022-08-17 10:08:24 -0700

    Update RIPE-Atlas-Install.sh

--- scripts/RIPE-Atlas-Install.sh
+++ scripts/RIPE-Atlas-Install.sh
@@ -6,3 +6,5 @@ git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git
 ./ripe-atlas-software-probe/build-config/debian/bin/make-deb
 sudo dpkg -i atlasswprobe-*.deb
 echo RXTXRPT=yes | sudo tee -a /var/atlas-probe/state/config.txt
+logger `cat /var/atlas-probe/etc/probe_key.pub`
+sudo service atlas restart
