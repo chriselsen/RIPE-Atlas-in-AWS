56e28030f666062c07e526bf53032cfeaf70680a
Christian Elsen <chriselsen@users.noreply.github.com>
2022-08-17 11:46:24 -0700

    Update RIPE-Atlas-Install.sh

--- scripts/RIPE-Atlas-Install.sh
+++ scripts/RIPE-Atlas-Install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update && 
-    sudo apt-get -o Dpkg::Options::="--force-confnew" upgrade -q -y --force-yes &&
-    sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -q -y --force-yes
+sudo apt update -y
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 sudo apt install git tar fakeroot libssl-dev libcap2-bin autoconf automake libtool build-essential -y --force-yes
 cd /tmp
 git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git
