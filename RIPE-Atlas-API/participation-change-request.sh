07f15aa7694fd2c1af143a3e2c0623c7e98ddcef
Christian Elsen <chriselsen@users.noreply.github.com>
2022-08-17 16:08:27 -0700

    Create participation-change-request.sh

---
+++ RIPE-Atlas-API/participation-change-request.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$api_key = "Your_API_KEY"
+mm_id="43940924"
+action="add"
+value="1004598,1004605,1004606,1004607,1004614,1004608,1004609,1004615,1004619,1004620,1004610,1004611,1004612,1004613,1004616,1004617,1004618"
+
+requested=`echo $value | tr -cd , | wc -c`
+change="[{\"action\":\""$action"\",\"requested\":"$((requested + 1))",\"type\":\"probes\",\"value\":\""$value"\"}]"
+curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d $change https://atlas.ripe.net:443/api/v2/measurements/$mm_id/participation-requests/?key=$api_key
