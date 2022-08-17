14dea1bc2b51b356781f1f14a3b82e01f5fbbe54
Christian Elsen <chriselsen@users.noreply.github.com>
2022-08-17 11:47:54 -0700

    Update RIPE-Atlas-Install.sh

--- scripts/RIPE-Atlas-Install.sh
+++ scripts/RIPE-Atlas-Install.sh
@@ -6,7 +6,7 @@ sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force
 sudo apt install git tar fakeroot libssl-dev libcap2-bin autoconf automake libtool build-essential -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 cd /tmp
 git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git
-./ripe-atlas-software-probe/build-config/debian/bin/make-deb
+sudo ./ripe-atlas-software-probe/build-config/debian/bin/make-deb
 sudo dpkg -i atlasswprobe-*.deb
 echo RXTXRPT=yes | sudo tee -a /var/atlas-probe/state/config.txt
 sudo service atlas restart
